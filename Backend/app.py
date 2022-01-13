c2ade97305b194802b43a33805bca07846c5ccb9
Bram Bossuyt2 <bram.bossuyt2@student.howest.be>
2022-01-13 11:01:42 +0100

    app.py setup

---
+++ Backend/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, jsonify
+from flask_socketio import SocketIO, send, emit
+from flask_cors import CORS
+import time
+import threading
+import configparser
+
+config = configparser.ConfigParser()
+config.read('Backend\config.ini')
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = config['app']['key']
+
+socketio = SocketIO(app, cors_allowed_origins="*")
+CORS(app)
+
+# API ENDPOINTS
+
+
+@app.route('/')
+def hello():
+    return "Server is running."
+
+# SOCKET IO
+
+
+@socketio.on('connect')
+def connect():
+    print('A new client connects')
+
+# RUN
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=False, host='0.0.0.0') # default port is 5000
