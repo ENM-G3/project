aa43aa4446fff4f679996f3f2ccdd70b56ed22fe
Jonas Faber <jonas.h.faber@gmail.com>
2022-01-18 11:23:04 +0100

    added first apexchart

--- Frontend/app/js/app.js
+++ Frontend/app/js/app.js
@@ -1,25 +1,26 @@
-import * as SocketIO from './lib/socket.io.min.js' ;
+import * as SocketIO from './lib/socket.io.min.js';
+import * as AC from './lib/apexcharts.min.js';
 import Socket from './SOCKET/index.js';
 import API from './API/index.js';
 
 
 export default class App {
-    _hostname = "localhost:5000";
-    _socketio = io(this.hostname);
-    _socket = new Socket(this);
+    _hostname = "localhost:5001";
+    //_socketio = io(this.hostname);
+    //_socket = new Socket(this);
     _api = new API(this);
 
     constructor() {
         this.init();
     }
 
-    get socketio() {
-        return this._socketio;
-    }
+    // get socketio() {
+    //     return this._socketio;
+    // }
 
-    get socket() {
-        return this._socket;
-    }
+    // get socket() {
+    //     return this._socket;
+    // }
 
     get api() {
         return this._api;
@@ -29,8 +30,32 @@ export default class App {
 
         // hier komen de listeners, structuur nog uit te zoeken
         console.log("App has been initialized!");
-        console.log();
-        console.log(await this.api.facts.getByType("weetje"));
+        let watthours = await this.api.history.get("Duiktank", "1d", "TotaalNet", "1h");
+        console.log(watthours);
+
+        let data = [], labels = [];
+
+        for (const hour of watthours.data) {
+            data.push(Math.round(hour._value));
+            labels.push(hour._time);
+        }
+
+        let myChart = new ApexCharts(document.querySelector("#test"), {
+            chart: {
+              type: 'area',
+            },
+            dataLabels: {
+                enabled: false
+            },
+            labels,
+            series: [{
+              data
+            }],
+            yaxis: {
+              opposite: true,
+            }
+          })
+          myChart.render();
         
     }
 
